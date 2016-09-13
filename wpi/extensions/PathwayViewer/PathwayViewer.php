69c150ffd249c9f7aee555d831eab2006ad3b190
www-data <nunomsilvanunes@gmail.com>
2016-09-13 13:24:00 +0200

    first commit after db upgrade

--- wpi/extensions/PathwayViewer/PathwayViewer.php
+++ wpi/extensions/PathwayViewer/PathwayViewer.php
@@ -1,8 +1,9 @@
 <?php
-require_once ( 'DetectBrowserOS.php' );
+require_once('DetectBrowserOS.php');
 
 /*
- * Loads an interactive pathway viewer using svgweb.
+ * Enable pvjs (interactive pathway viewer/editor)
+ * Used in both pathway page and widget.
  */
 
 $wgExtensionFunctions[] = 'wfPathwayViewer';
@@ -10,7 +11,7 @@ $wgHooks['LanguageGetMagic'][]  = 'wfPathwayViewer_Magic';
 
 function wfPathwayViewer() {
 	global $wgParser;
-	$wgParser->setFunctionHook( "PathwayViewer", "displayPathwayViewer" );
+	$wgParser->setFunctionHook( "PathwayViewer", "PathwayViewer::enable" );
 }
 
 function wfPathwayViewer_Magic( &$magicWords, $langCode ) {
@@ -18,60 +19,57 @@ function wfPathwayViewer_Magic( &$magicWords, $langCode ) {
 	return true;
 }
 
-function displayPathwayViewer(&$parser, $pwId, $imgId) {
-	global $wpiJavascriptSources, $jsRequireJQuery, $wgRequest;
-
-	$jsRequireJQuery = true;
-
-	try {
-		$parser->disableCache();
-
-		//Add javascript dependencies
-		// TODO it appears the following line only adds JS dependencies for the XrefPanel, which
-		// we are no longer using, so I think the XrefPanel code should be removed. --AR
-		XrefPanel::addXrefPanelScripts();
-		$wpiJavascriptSources = array_merge( $wpiJavascriptSources,
-			PathwayViewer::getJsDependencies() );
-
-		$revision = $wgRequest->getval('oldid');
-
-		$pathway = Pathway::newFromTitle($pwId);
-		if ($revision) {
-			$pathway->setActiveRevision($revision);
-		}
-		$png = $pathway->getFileURL(FILETYPE_PNG);
-				$gpml = $pathway->getFileURL(FILETYPE_GPML);
-
-			   $script = "<script>var gpmlFilePath = \"$gpml\"; var pngFilePath = \"$png\";</script>";
-		return array($script, 'isHTML'=>1, 'noparse'=>1);
-	} catch ( Exception $e ) {
-		return "invalid pathway title: $e";
-	}
-	return true;
-}
-
 class PathwayViewer {
 	static function getJsDependencies() {
 		global $wgScriptPath;
 
-		if (preg_match('/(?i)msie [6-8]/', $_SERVER['HTTP_USER_AGENT'])) {
+		if(preg_match('/(?i)msie [6-8]/',$_SERVER['HTTP_USER_AGENT'])) {
 			// if IE<=8
 			$scripts = array(
 			);
 		}
 		else {
-			// if IE>8 or any other browser
+			// if IE>8
 			$scripts = array(
-				"$wgScriptPath/wpi/js/querystring-parameters.js",
-				"$wgScriptPath/wpi/extensions/PathwayViewer/pathwayviewer.js",
-				// libs required by pathwayviewer.js
-				"//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js",
-				"//cdnjs.cloudflare.com/ajax/libs/modernizr/2.7.1/modernizr.min.js",
-				// pvjs
-				"$wgScriptPath/wpi/lib/pathvisiojs/js/pathvisiojs-2.2.0.bundle.min.js",
+				// What are these for?
+				"$wgScriptPath/wpi/js/jquery/plugins/jquery.mousewheel.js",
+				"$wgScriptPath/wpi/js/jquery/plugins/jquery.layout.min-1.3.0.js",
+				// pvjs and dependencies
+				"//cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js",
+				"//mithril.js.org/archive/v0.2.2-rc.1/mithril.min.js",
+				// TODO remove the polyfill bundle below once the autopolyfill
+				// work is complete. Until then, leave it as-is.
+				"$wgScriptPath/wpi/lib/pvjs/release/polyfills.bundle.min.js",
+				"$wgScriptPath/wpi/lib/pvjs/release/pvjs.core.min.js",
+				"$wgScriptPath/wpi/lib/pvjs/release/pvjs.custom-element.min.js",
+				// JS to trigger Java webstart for currently visited pathway
+				"https://www.java.com/js/deployJava.js",
+				"$wgScriptPath/wpi/extensions/PathwayViewer/pathwayviewerJavaWebStartLauncher.js",
 			);
 		}
 
 		return $scripts;
 	}
+
+	static function enable(&$parser, $pwId, $imgId) {
+		global $wgOut, $wgStylePath, $wpiJavascriptSources, $wgScriptPath,
+			$wpiJavascriptSnippets, $jsRequireJQuery, $wgRequest, $wgJsMimeType;
+
+		$jsRequireJQuery = true;
+
+		try {
+			$wpiJavascriptSources = array_merge($wpiJavascriptSources, PathwayViewer::getJsDependencies());
+
+			$revision = $wgRequest->getval('oldid');
+
+			$pathway = Pathway::newFromTitle($pwId);
+
+			if($revision) {
+				$pathway->setActiveRevision($revision);
+			}
+		} catch(Exception $e) {
+			return "invalid pathway title: $e";
+		}
+	}
+
 }
