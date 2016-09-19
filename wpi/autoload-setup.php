d28d15a000bcbc6b891420a995f0725e43a448cb
JonathanMELIUS <meliusjonathan@gmail.com>
2016-09-19 10:50:17 +0200

    Add TissueAnalyzer files

--- wpi/autoload-setup.php
+++ wpi/autoload-setup.php
@@ -90,6 +90,7 @@ require_once( "$IP/wpi/extensions/ContributionScores/ContributionScores.php" );
 require_once( "$IP/wpi/extensions/PullPages/PullPages.php" );
 require_once( "$IP/wpi/DetectBrowserOS.php");
 require_once( "$IP/wpi/search.php");
+require_once( "$IP/wpi/extensions/TissueAnalyzer/TissueAnalyzer.php" );
 
 //Register the default organisms
 Organism::registerDefaultOrganisms();
