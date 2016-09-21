5519b8da01618b16a7d6176f9d82c983720d67fc
AlexanderPico <alex.pico@gladstone.ucsf.edu>
2016-09-21 23:01:42 +0200

    initial work on #20 PV Launch

--- wpi/extensions/PathwayViewer/pathwayviewerJNLPGenerator.php
+++ wpi/extensions/PathwayViewer/pathwayviewerJNLPGenerator.php
@@ -20,20 +20,25 @@
   }
 
   // see http://schema.org/version
-  if (isset($_GET['version'])) {
-    $version = htmlspecialchars($_GET['version']);
-  }
-  else {
-    $version = 0;
-  }
+//  if (isset($_GET['version'])) {
+//    $version = htmlspecialchars($_GET['version']);
+//  }
+//  else {
+//    $version = 0;
+//  }
 
   // XML JNLP
   $template_path = 'http://www.pathvisio.org/data/releases/current/webstart/pathvisio.jnlp';
   $jnlp = simplexml_load_file($template_path);
+  $jnlp['codebase'] = 'http://upgrade.wikipathways.org/w/wpi/extensions/PathwayViewer/';
+  $jnlp['href'] = 'pathwayviewerJNLPGenerator.php?identifier='.$identifier;
+  $resources_el = $jnlp->{'resources'};
+  $jar_el = $resources_el->{'jar'};
+  $jar_el['href'] = 'http://www.pathvisio.org/data/releases/current/webstart/pathvisio.jar';
   $application_desc_el = $jnlp->{'application-desc'};
-  $application_desc_el->argument[0] = '-wp';
+  $application_desc_el->argument[0] = '-wpid';
   $application_desc_el->argument[1] = $identifier;
-  $application_desc_el->argument[2] = '-wprev';
-  $application_desc_el->argument[3] = $version;
+  //$application_desc_el->argument[2] = '-wprev';
+  //$application_desc_el->argument[3] = $version;
   echo $jnlp->asXML();
 ?>
